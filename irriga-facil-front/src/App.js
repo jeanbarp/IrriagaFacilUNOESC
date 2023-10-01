d1b2548fc8c3c7412dc078668658a9510302e548
Pedro Vinícius <62823394+pedrovinis@users.noreply.github.com>
2023-10-01 19:59:28 -0300

    feat: start-and-configure-route

--- irriga-facil-front/src/App.js
+++ irriga-facil-front/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./routes/home"
 
 function App() {
   return (
-    <div className="App">
-      Projeto Irriga Fácil
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ irriga-facil-front/src/routes/home.jsx
@@ -0,0 +1,5 @@
+
+
+export default function Home() {
+  return <div>Projeto Irriga Facil</div>
+}
